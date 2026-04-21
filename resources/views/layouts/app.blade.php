dc7ea68e1c0fc691718ec25f02b81a899539a378
Xena <atharizkysena@gmail.com>
2026-04-21 08:38:07 +0700

    Update Navbar dan footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     <nav class="bg-white fixed w-full z-20 top-0 start-0 border-b border-default py-1">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="{{ route('home') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <span class="self-center text-xl text-heading font-semibold whitespace-nowrap">CLOTHIFY</span>
+                <span class="self-center text-xl text-heading font-extrabold whitespace-nowrap">CLOTHIFY</span>
             </a>
 
             <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
@@ -44,7 +44,7 @@
                 <!-- Dropdown menu -->
                 <div class="z-50 hidden bg-white border border-default-medium rounded-base shadow-lg w-44" id="user-dropdown">
                     <div class="px-4 py-3 text-sm border-b border-default">
-                        <span class="block text-heading font-medium">Affatha Rizky Sena</span>
+                        <a href="{{route('login')}}" class="block text-heading font-medium">Login</a>
                     </div>
                     <ul class="p-2 text-sm text-body font-medium" aria-labelledby="user-menu-button">
                         <li>
@@ -53,7 +53,8 @@
                     </ul>
                 </div>
                 <button data-collapse-toggle="navbar-user" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body 
-                 rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-neutral-tertiary" aria-controls="navbar-user" aria-expanded="false">
+                 rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-neutral-tertiary"
+                    aria-controls="navbar-user" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
                     <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14" />
@@ -61,15 +62,34 @@
                 </button>
             </div>
             <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
-                <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-default rounded-base bg-neutral-secondary-soft md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-neutral-primary">
+                <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-default rounded-base bg-neutral-secondary-soft 
+                 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-neutral-primary">
                     <li>
-                        <a href="{{ route('produk') }}" class="block py-2 px-3 text-heading rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent">PRODUK</a>
+                        <a href="{{ route('produk') }}"
+                            class="group relative py-2 px-3 text-heading font-bold transition-transform duration-200 hover:-translate-y-0.5 text-xs">
+                            PRODUK
+                            <!-- Hover Effect -->
+                            <span class="absolute bottom-0 left-1/2 -translate-x-1/2 w-8 h-0.5
+                             bg-gray-900 opacity-0 group-hover:opacity-100 group-hover:bottom-1 transition-all duration-300"></span>
+                        </a>
                     </li>
+
                     <li>
-                        <a href="#" class="block py-2 px-3 text-heading rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent">GALERI</a>
+                        <a href="{{ route('galeri') }}"
+                            class="group relative py-2 px-3 text-heading font-bold transition-transform duration-200 hover:-translate-y-0.5 text-xs">
+                            GALERI
+                            <span class="absolute bottom-0 left-1/2 -translate-x-1/2 w-8 h-0.5
+                             bg-gray-900 opacity-0 group-hover:opacity-100 group-hover:bottom-1 transition-all duration-300"></span>
+                        </a>
                     </li>
+
                     <li>
-                        <a href="#" class="block py-2 px-3 text-heading rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent">TENTANG</a>
+                        <a href="{{ route('tentang') }}"
+                            class="group relative py-2 px-3 text-heading font-bold transition-transform duration-200 hover:-translate-y-0.5 text-xs">
+                            TENTANG
+                            <span class="absolute bottom-0 left-1/2 -translate-x-1/2 w-8 h-0.5
+                             bg-gray-900 opacity-0 group-hover:opacity-100 group-hover:bottom-1 transition-all duration-300"></span>
+                        </a>
                     </li>
                 </ul>
             </div>
@@ -114,6 +134,7 @@
             <p>Keranjang Anda masih kosong.</p>
         </div>
     </div>
+
 
 </body>
 
