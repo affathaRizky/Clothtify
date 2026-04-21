af40de0f9f20f19c29e4ffa6b94be25e07bbe9a8
Xena <atharizkysena@gmail.com>
2026-04-21 08:45:32 +0700

    Menambahkan Routing halaman pada navlink -> Home,Produk,Galeri,Tentang

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', function () {
-    return view('dashboard');
+    return view('home');
 });
+
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/produk', function () {
+    return view('produk');
+})->name('produk');
 
 Route::get('/login', function () {
     return view('login');
-});
+})->name('login');
+
+Route::get('/lupaPassword', function () {
+    return view('lupaPassword');
+})->name('lupaPassword');
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
+
+Route::get('/tentang', function () {
+    return view('tentang');
+})->name('tentang');
+
+Route::get('/galeri', function () {
+    return view('galeri');
+})->name('galeri');
