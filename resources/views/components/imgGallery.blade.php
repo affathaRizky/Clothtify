df7aa8ed6b9d9d5420f44441d5eb4c289099728a
citramurniatisiregar14-commits <citramurniatisiregar14@gmail.com>
2026-04-28 10:46:54 +0700

    Components untuk img pada page gallery

---
+++ resources/views/components/imgGallery.blade.php
@@ -0,0 +1,4 @@
+<div class="overflow-hidden rounded-xl shadow-md group">
+    <img src="{{ $image }}"
+        class="w-full aspect-[3/4] object-cover group-hover:scale-110 transition duration-300">
+</div>
