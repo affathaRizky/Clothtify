0e063d66fb00e6b21c825578bc52af992617481b
Xena <atharizkysena@gmail.com>
2026-04-28 09:38:19 +0700

    Update Homepage mengganti extends layout serta menambahkan Custom title

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.main')
+
+@section('title', 'Home - CLOTHIFY')
+
+@section('content')
+
+<!-- HERO -->
+<section class="relative w-full h-screen overflow-hidden">
+    <img src="{{ asset('images/BG_HOME_PBL.jpg') }}" alt="Background Image"
+        class="absolute inset-0 w-full h-full object-cover object-center">
+    <div class="absolute inset-0 bg-black/40"></div>
+    <div class="relative z-10 flex items-center justify-center h-full text-white text-center">
+        <div>
+            <a href="{{ route('product') }}" class="border-2 border-white px-8 py-3 rounded-full hover:bg-white hover:text-black transition duration-300 font-bold">
+            SHOP NOW
+            </a>
+        </div>
+    </div>
+</section>
+
+@endsection
