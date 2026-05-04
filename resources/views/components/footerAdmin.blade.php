07a2b305b0895cb5f44314c3870a178e61a34544
Xena <atharizkysena@gmail.com>
2026-05-04 12:37:43 +0700

    Footer For ADmin Pages V1

---
+++ resources/views/components/footerAdmin.blade.php
@@ -0,0 +1,7 @@
+<!-- Footer Admin -->
+<footer class="bg-[#1a1a1a] text-white pt-8 pb-6">
+    <hr class="border-gray-700 my-6" />
+    <div class="text-center text-sm text-gray-500 py-0">
+        © {{ date('Y') }} <a href="#" class="hover:text-white transition">Clothify™</a>. All Rights Reserved.
+    </div>
+</footer>
