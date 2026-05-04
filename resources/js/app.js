c5d372c98a5c9802a971d160a538d9b26cac606f
Xena <atharizkysena@gmail.com>
2026-05-04 19:53:42 +0700

    Import Vanilla Js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,5 +2,5 @@ import './bootstrap';
 import '../css/app.css';
 import 'flowbite';
 import './sidebarCart.js';
-import './ProfileDrop.js';
 import './resendOtpCountdown.js';
+import './productDetail.js';
