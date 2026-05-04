fed1b0771b3729882118e1648653cd981fee2319
Xena <atharizkysena@gmail.com>
2026-05-04 12:36:49 +0700

    Header For Guest/User Pages Version 2

---
+++ resources/views/components/headerV2.blade.php
@@ -0,0 +1,8 @@
+<!-- NAVBAR, CHECKOUT PRODUCTS -->
+<header class="flex items-center justify-between px-4 py-3 border-b border-gray-200 sticky top-0 bg-white z-10">
+    <a href="javascript:history.back()" class="p-2 text-gray-600 hover:text-gray-900 transition">
+        <i class="fa-solid fa-arrow-left text-lg"></i>
+    </a>
+    <h1 class="font-playfair italic text-xl font-semibold tracking-wide">Clothify</h1>
+    <div class="w-10"></div>
+</header>
