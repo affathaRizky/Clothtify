2577bd4d42665b3a9e7f9644db77448c94918aee
Xena <atharizkysena@gmail.com>
2026-06-15 10:19:31 +0700

    Database Connection to pembeli table

---
+++ app/Models/pembeli.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pembeli extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $table = 'pembeli';
+
+    protected $primaryKey = 'id_pembeli';
+}
